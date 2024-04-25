06359466174f14a3437f35b2310ad730e7ecd8de
Alec <alec.ritzrow@gmail.com>
2024-04-24 23:39:34 -0800

    js basics

--- proj1/index.js
+++ proj1/index.js
@@ -1,11 +1,42 @@
-//first js code
+// first js code
+// dyanamic language
 console.log('hello world');
 
+// primitive/value types
 let name = 'AL'; // string literal
 let age = 23; // number literal
 let isApproved = true; // boolean literal
 let und = undefined;
-let undef = null;
+let undef = null; // clear value of a variable
+
+// reference types
+function greet(name, lastName) { // function performing a task
+    console.log('hello ' + name + ' ' + lastName);
+}
+greet('John', 'Smith');
+// calculating a value
+function square(number) {
+    return number*number;
+}
+console.log(square(2));
+
+let selectedColors = ['red', 'blue']; // array literal
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+
+let person = {
+    name: 'AL',
+    age: 23
+}; // object literal
+
+// dot notation
+person.name = 'LA'
+
+// bracket notation
+let selection = 'name';
+person[selection] = 'BOB';
+
+console.log(person.name);
 
 console.log(name);
 // variables cannot be reserved keywords
@@ -21,4 +52,3 @@ let lastName = 'Ritzrow';
 // constant variable: const interestRate = 0.3;
 // normal variable: let interestRate = 1;
 // console.log(interestRate);
-
