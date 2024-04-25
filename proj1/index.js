cc90aea86555ebee0e11b759c25bfc9750f315c1
Alec <alec.ritzrow@gmail.com>
2024-04-24 23:02:17 -0800

    learning

--- proj1/index.js
+++ proj1/index.js
@@ -1,2 +1,24 @@
 //first js code
 console.log('hello world');
+
+let name = 'AL'; // string literal
+let age = 23; // number literal
+let isApproved = true; // boolean literal
+let und = undefined;
+let undef = null;
+
+console.log(name);
+// variables cannot be reserved keywords
+// should be meaningful
+// cannot start with a number
+// cannot contain a space or hyphen
+// are case sensitive
+
+let firstName = 'Alec';
+let lastName = 'Ritzrow';
+// camelCase
+
+// constant variable: const interestRate = 0.3;
+// normal variable: let interestRate = 1;
+// console.log(interestRate);
+
